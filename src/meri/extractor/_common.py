33f3c58441acc3a3350776c546ce07f4aa25ceed
Teemu Autto <teemu@rautakuu.org>
2024-10-16 22:27:15 +0000

    chore: Expanded method documentation.

--- src/meri/extractor/_common.py
+++ src/meri/extractor/_common.py
@@ -9,11 +9,19 @@ logger = get_logger(__name__)
 
 class PolynomialDelayEstimator:
     """
+    A polynomial delay estimator that estimates the delay between articles based on the time of day.
+
     `sklearn.linear_model.LinearRegression` is used to fit a polynomial regression model to the data. The model is then
     used to estimate the delay between articles based on the time of day.
     """
 
     def __init__(self, coefficients: list[float], intercept: float):
+        """
+        Initialize the polynomial delay estimator.
+
+        :param coefficients: The coefficients of the polynomial regression model.
+        :param intercept: The intercept of the polynomial regression - i.e. the value of the polynomial when x=0.
+        """
         self.coefficients = coefficients
         self.intercept = intercept
 
