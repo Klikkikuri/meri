9507de04a33954dd0e6120461d8f3ca27f837e20
Teemu Autto <teemu@rautakuu.org>
2024-10-26 19:49:06 +0000

    Beginning of stakeholder evaluation. Uses haystack for LLM.

--- src/meri/abc.py
+++ src/meri/abc.py
@@ -1,11 +1,11 @@
 from datetime import datetime, timedelta
 from re import Pattern
-from typing import Optional
+from typing import List, Optional
 from urllib.parse import ParseResult
 
 import newspaper
 from opentelemetry import trace
-from pydantic import AnyHttpUrl
+from pydantic import AnyHttpUrl, BaseModel, Field
 from structlog import get_logger
 
 logger = get_logger(__name__)
@@ -63,3 +63,52 @@ class Outlet:
         :param url: The URL of the article.
         """
         raise NotImplementedError
+
+
+class VestedGroup(BaseModel):
+    """
+    Model for identified interest group, person, or entity.
+    """
+    name: str = Field(..., description="Name of the group, person, or entity that has vested interest.")
+    questions: List[str] = Field(
+        ...,
+        description="A list of questions aimed at uncovering the vested interest. Each question should explicitly identify the party and include a complete noun phrase.",
+        examples=[
+            "What is <individual name>'s stance as <role or affiliation> on <related issue from article>?",
+            "Why might <organization name> have a vested interest in the coverage of <related issue from article>?",
+            "How does <affected group> view the impact of <issue discussed in the article>?",
+            "What is the potential bias of <interviewed person or organization> regarding <related issue from article>?",
+            "What role does <lobbying group or business entity> play in influencing public opinion on <topic>?",
+            "How does <individual or group> benefit from public perception of <issue>?",
+            "Why was <organization> included in the discussion of <issue>?",
+            "What possible interests does <corporation or interest group> have in the outcomes of <related topic>?",
+        ],
+    )
+    reasoning: str = Field(..., description="Explanation of why the entity is likely to have a vested interest in the issue.")
+
+
+class ArticleContext(BaseModel):
+    """
+    Response from the vested interest extraction model.
+
+    Example items in `<angle brackets>` are placeholders for actual values.
+    """
+    wikipedia_keywords: List[str | None] = Field(
+        [],
+        description="List of (Wikipedia) article keywords that helps to understand the context of the article.",
+        examples=[
+            "Elinkeinoelämän keskusliitto",
+            "Työnantajajärjestöt",
+            "Kansallinen Kokoomus",
+            "Jyri Häkämies",
+            "Lobbaus Suomessa",
+            "Kolmikantainen yhteistyö",
+            "Victoria (Ruotsin kruununprinsessa)",
+            "<locality>",
+            "<attitude towards issue> in <locality>",
+            "<notable person>",
+            "<notable organization>",
+            "<notable event>",
+        ],
+    )
+    groups: List[VestedGroup] = Field([], description="List of entities identified from the article.")
---
+++ src/meri/llm.py
@@ -0,0 +1,160 @@
+from enum import Enum
+from importlib.resources import files
+import json
+import logging
+from pathlib import Path
+from typing import Annotated, List, Optional, TypeVar, Type, get_args, get_origin
+
+import newspaper
+from pydantic import AnyHttpUrl, BaseModel, ValidationError
+
+from .extractor._processors import MarkdownStr, _get_from_stack
+from .settings import settings
+from .abc import ArticleContext, VestedGroup
+
+from platformdirs import user_data_dir
+
+from haystack import Pipeline, component
+from haystack.components.generators import OpenAIGenerator
+from haystack.components.builders import PromptBuilder
+
+LLM_INST_GEN_JSON = r"""
+Respond by providing a reasoning first.
+Then, you MUST format response following this JSON schema, but only return the actual instances without any additional schema definition:
+```json
+{{schema}}
+```
+"""
+
+LLM_INST_USE_LANGUAGE = {
+    "en": "Write the response in English (British).",
+    "fi": "Kirjoita vastaus suomeksi.",
+}
+
+PROMPT_TEMPLATE_VESTED_GROUPS = "vested_groups_inst.md.j2"
+
+RE_JSON_BLOCK = r"```json\n(.*?)\n```"
+
+class LLMGenerators(Enum):
+    OPENAI = "OpenAIGenerator"
+
+
+logger = logging.getLogger(__name__)
+
+
+def get_prompt_template(template_name) -> str:
+    """
+    Get the prompt text based on the template name.
+
+    Searches for the prompt template in the user data directory first, then in the package data directory.
+    """
+
+    PROMPT_ENCODING = "utf-8"
+
+    prompt_file_name = PROMPT_TEMPLATE_VESTED_GROUPS
+    user_prompt_dir = Path(user_data_dir(__package__), "prompts")
+
+    user_prompt_file = user_prompt_dir / prompt_file_name
+    if user_prompt_file.exists():
+        return user_prompt_file.read_text(encoding=PROMPT_ENCODING)
+
+    # Check from package data directory
+    resource = f"prompts/{prompt_file_name}"
+    return files(__package__).joinpath(resource).read_text(encoding=PROMPT_ENCODING)
+
+
+def get_generator():
+    return OpenAIGenerator()
+
+
+@component
+class ModelOutputValidator:
+    """
+    
+    Based on <https://haystack.deepset.ai/tutorials/28_structured_output_with_loop>
+    """
+    def __init__(self, pydantic_model: BaseModel):
+        self.pydantic_model = pydantic_model
+        self.iteration_counter = 0
+
+    # Define the component output
+    @component.output_types(valid_replies=List[str], invalid_replies=Optional[List[str]], error_message=Optional[str])
+    def run(self, replies: List[str]):
+        self.iteration_counter += 1
+
+        ## Try to parse the LLM's reply ##
+        # If the LLM's reply is a valid object, return `"valid_replies"`
+        try:
+            response = extract_json(replies[0])
+            if response is None:
+                raise ValueError("No JSON block found in the response.")
+            self.pydantic_model.model_validate_json(response)
+            logger.debug("OutputValidator at Iteration %d: Valid JSON from LLM - No need for looping", self.iteration_counter, extra={"replies": replies})
+            return {"valid_replies": replies}
+
+        # If the LLM's reply is corrupted or not valid, return "invalid_replies" and the "error_message" for LLM to try again
+        except (ValueError, ValidationError) as e:
+            logger.warning(
+                "OutputValidator at Iteration %d: Invalid JSON from LLM - Let's try again.\n"
+                "Output from LLM:\n %s \n"
+                "Error from OutputValidator: %s",
+                self.iteration_counter, replies[0], e
+            )
+            return {"invalid_replies": replies, "error_message": str(e)}
+
+
+def extract_interest_groups(stack: List):
+    """
+    Extract interest groups from text.
+    """
+    if settings.DEBUG:
+        logging.getLogger("canals.pipeline.pipeline").setLevel(logging.DEBUG)
+
+    template = get_prompt_template("vested_groups_inst")
+    template += LLM_INST_GEN_JSON
+
+    article = _get_from_stack(stack, newspaper.Article)
+    title = article.title
+    url = article.original_url
+    text = _get_from_stack(stack, MarkdownStr)
+
+    content_lang = article.meta_lang or "en"
+    if content_lang in LLM_INST_USE_LANGUAGE:
+        template += LLM_INST_USE_LANGUAGE[content_lang]
+
+    prompt_builder = PromptBuilder(template)
+    llm = get_generator()
+    output_validator = ModelOutputValidator(pydantic_model=ArticleContext)
+
+    p = Pipeline(max_runs_per_component=1)
+    p.add_component("prompt_builder", prompt_builder)
+    p.add_component("llm", llm)
+    p.add_component(instance=output_validator, name="output_validator")
+
+    p.connect("prompt_builder", "llm")
+    p.connect("llm", "output_validator")
+    # If a component has more than one output or input, explicitly specify the connections:
+    p.connect("output_validator.invalid_replies", "prompt_builder.invalid_replies")
+    p.connect("output_validator.error_message", "prompt_builder.error_message")
+
+    results = p.run({
+        "prompt_builder": {
+            "url": str(url),
+            "title": str(title),
+            "text": str(text),
+            "schema": ArticleContext.schema_json(indent=2),
+        }
+    })
+
+    return results
+
+
+def extract_json(response: str):
+    """
+    Extract JSON from response.
+    """
+    import re
+    m = re.search(RE_JSON_BLOCK, response, flags=re.MULTILINE | re.DOTALL)
+    if m:
+        return m.group(1)
+    return None
