2e3d06ec50fbf8485846a55c888fbda1b1fd6cdb
Teemu Autto <teemu@rautakuu.org>
2025-04-25 09:15:26 +0000

    Working PoC for structured wikipedia article sectionizer.

--- src/meri/wp.py
+++ src/meri/wp.py
@@ -7,35 +7,35 @@ Wikipedia page, and formats them into sections.
 TODO: Make into context aware splitter
 """
 
-from copy import copy, deepcopy
+from copy import deepcopy
 import re
-from haystack.components.preprocessors import DocumentSplitter
 from haystack import Document
+import mwclient.page
 
 from meri.utils import detect_language
 
 import getpass
 import logging
 import os
-from typing import Any, Dict, Iterator, List, Optional, Set, Tuple, TypedDict
-from dotenv import load_dotenv
+from typing import Any, Callable, Dict, Iterator, List, Optional, Set, Tuple, TypedDict
 
 import mwclient
-import mwparserfromhell
 
 from trafilatura import load_html
 from trafilatura.htmlprocessing import prune_unwanted_nodes
-from lxml.etree import XPath
+from lxml.etree import XPath  # nosec
 from trafilatura.htmlprocessing import tree_cleaning, convert_tags
 from trafilatura.core import Extractor
 from trafilatura.xml import xmltotxt
 
 from .scraper import get_user_agent
 
+
 class WikiDocumentMeta(TypedDict, total=False):
     url: str
     title: str
     summary: str
+
 
 logger = logging.getLogger(__name__)
 
@@ -64,101 +64,183 @@ SECTIONS_TO_IGNORE: Dict[str, Set] = {
     "fi": {
         "Lähteet",
         "Aiheesta muualla",
-    }
+    },
 }
 
 
-def html_to_markdown(html: str, extractor_args = {}) -> str:
+def fetch_revision(page: mwclient.page.Page) -> Document:
     """
-    Special wikipedia-specific HTML to Markdown conversion.
+    Fetch wikipedia page revision.
+
+    Constructs a html document from the page revision, and converts it to markdown using :func:`html_to_markdown`.
+    """
+
+    if not page.exists or not page.revision:
+        raise ValueError("Page does not exist or has no revision.")
+
+    params = {
+        "oldid": page.revision,
+        "format": "json",
+        "prop": "text|headhtml",
+        "disablelimitreport": True,
+        "disableeditsection": True,
+        "mobileformat": False,
+        "contentmodel": "wikitext",
+        "disabletoc": True,
+    }
+
+    data = page.site.get("parse", **params)
+    if not data or "error" in data:
+        raise ValueError("Failed to fetch page revision. Error: %s" % data.get("error", "Unknown error"))
+    if "parse" not in data:
+        raise ValueError("Failed to fetch page revision - no parse data found.")
+
+    # Construct the HTML document, trafilatura expect document to have a <body>.
+    html = "\n".join(
+        [
+            data["parse"]["headhtml"]["*"],
+            '<main id="content" class="mw-body"><div id="bodyContent" class="content">',
+            f'<h1>{data["parse"]["title"]}</h1>',
+            data["parse"]["text"]["*"],
+            "</div></main>",
+            "</body></html>",
+        ]
+    )
+
+    doc = Document(
+        content=html,
+        meta={
+            "title": page.base_title,
+            "language": page.pagelanguage,
+        },
+    )
+
+    return doc
+
+
+def structured_html_to_markdown(doc: Document, extractor_args={}) -> Document:
+    """
+    Special Wikipedia-optimized HTML to Markdown conversion.
 
     This function uses trafilatura to convert HTML to Markdown, and then
     performs some additional cleanup to remove unwanted elements and
-    formatting.
+    formatting, specific to wikipedia pages.
     """
     from trafilatura.settings import DEFAULT_CONFIG
+
     config = deepcopy(DEFAULT_CONFIG)
 
     # Not really needed, but set it to avoid warnings
     config["DEFAULT"].setdefault("USER_AGENTS", get_user_agent())
 
     _extractor_args = {
-        'config': config,
-        'output_format': 'markdown',
-        'formatting': True,
-        'links': True,
-        'images': False,
-        'tables': True,
-        'comments': False
+        "config": config,
+        "output_format": "markdown",
+        "formatting": True,
+        "links": True,
+        "images": False,
+        "tables": True,
+        "comments": False,
     }
     _extractor_args.update(extractor_args)
 
-    doc = load_html(html)
-    if doc is None:
-        logger.warning("Failed to load HTML document.")
+    logger.debug("Extractor args: %s", _extractor_args)
+
+    html = load_html(doc.content)
+    if html is None:
         raise ValueError("Failed to load HTML document.")
 
     # Prune unwanted nodes
-    doc = prune_unwanted_nodes(doc, [XPath(x) for x in [
-        "//script", "//noscript", "//style", "//link", "//meta", "//form", "//input", "//button",
-        '//*[contains(@class, "noprint") or contains(@class, "ambox-notice")]',  # Remove "noprint" classed content
-    ]])
+    html = prune_unwanted_nodes(
+        html,
+        [
+            XPath(x)
+            for x in [
+                "//script",
+                "//noscript",
+                "//style",
+                "//link",
+                "//meta",
+                "//form",
+                "//input",
+                "//button",
+                '//*[contains(@class, "noprint") or contains(@class, "ambox-notice")]',  # Remove "noprint" classed content
+            ]
+        ],
+    )
 
-    # Remove links for pages that don't exist
-    doc = prune_unwanted_nodes(doc, [XPath(x) for x in [
-        '//*[contains(@class, "mw-redirect") or contains(@class, "new")]',  # Remove "mw-redirect" and "new" classed content
-    ]])
+    # # Remove links for pages that don't exist
+    html = prune_unwanted_nodes(
+        html,
+        [
+            XPath(x)
+            for x in [
+                # Remove "mw-redirect" and "new" classed content
+                '//a[contains(@class, "mw-redirect") or contains(@class, "new")]',
+            ]
+        ],
+    )
 
     options = Extractor(**_extractor_args)
 
-    doc = tree_cleaning(doc, options)
-    doc = convert_tags(doc, options)
-    txt = xmltotxt(doc.body, options.formatting)
+    html = convert_tags(html, options)
+    # doc = tree_cleaning(doc, options)
+    txt = xmltotxt(html.body, options.formatting)
 
     # Cleaup hacks
     # Remove citation links
-    txt = re.sub(r'\[\s*\d+\]', '', txt)
+    txt = re.sub(r"\[\s*\d+\]", "", txt)
     # Clean whitespace before word boundaries, left by some inline tags from trafilatura (e.g. <i> <b>)
-    txt = re.sub(r'\s+([.,;:!?)])', r'\1', txt)
+    txt = re.sub(r"\s+([.,;:!?)])", r"\1", txt)
 
-    return txt
+    new_doc = deepcopy(doc)
+    new_doc.content = txt
+
+    # Set canonical URL if available
+    if canon_url := XPath('//link[@rel="canonical"]/@href')(html):
+        new_doc.meta["url"] = canon_url[0]
+        new_doc.id = new_doc.meta.get("url", None)
+
+    return new_doc
 
 
-class BetterMarkdownChunker:
+class SectionNode(TypedDict):
+    level: int
+    title: str
+    summary: Optional[str]
+    body: Optional[str]
+    start: int
+    end: int
+    children: List["SectionNode"]
 
-    class SectionNode(TypedDict):
-        level: int
-        title: str
-        summary: Optional[str]
-        body: Optional[str]
-        start: int
-        end: int
-        children: List["MarkdownChunker.SectionNode"]
 
+class MarkdownChunker:
+    """
+    Parses a Markdown document into sections.
+    """
 
     def __init__(self, content: str, language: Optional[str] = None):
         """
         Initialize the MarkdownChunker.
 
-        Args:
-            content (str): Markdown document as a single string.
-            language (str): Language code for localized ignored sections.
+        :param content: The Markdown content to parse.
+        :param language: Language code of the content. If not provided, detection is attempted.
         """
+
         self.content = content
         self.language = language or detect_language(content)
-        self.ignored_sections = SECTIONS_TO_IGNORE.get(language, set())
+        self.ignored_sections: Set | List = SECTIONS_TO_IGNORE.get(language, set())
         self.heading_pattern = re.compile(r"^(#{1,6})\s+(.*)", re.MULTILINE)
-        self.doc: "BetterMarkdownChunker.SectionNode"
 
-    def parse(self) -> "BetterMarkdownChunker.SectionNode":
+        # Set the root node of the document. Has a side effect that only one main level heading is allowed.
+        self.doc: SectionNode
+
+    def parse(self) -> SectionNode:
         """
         Parses the Markdown content into hierarchical sections with metadata.
         Filters out ignored or empty sections.
 
         ..todo:: Implement LRU cache for the parsed document
-
-        Args:
-            content (str): The Markdown content to parse.
         """
         content = self.content
         matches = list(self.heading_pattern.finditer(content))
@@ -199,13 +281,12 @@ class BetterMarkdownChunker:
                 continue
 
             # Ensure correct parent-child relationship
-            while stack and stack[-1]['level'] >= level:
+            while stack and stack[-1]["level"] >= level:
                 stack.pop()
 
             # Add the new section to the current parent
             if stack:
-                stack[-1]['children'].append(section)
+                stack[-1]["children"].append(section)
             stack.append(section)
 
         return stack[0]  # Return the root node
-
