7d8ba08bae147114803ba0e8d2faf140f6794bff
Teemu Autto <teemu@rautakuu.org>
2025-05-01 10:51:03 +0000

    chore: Update Ollama API base URL detection and model retrieval

--- src/meri/settings/llms.py
+++ src/meri/settings/llms.py
@@ -22,37 +22,6 @@ class MissingGeneratorError(ImportError):
     pass
 
 
-def detect_generators(values: dict):
-    """
-    Detect the generator settings based on the environment variables or provided values.
-
-    This function checks for the presence of environment variables or values in the
-    provided dictionary to determine the appropriate generator settings.
-    It sets the default provider based on the detected settings.
-    """
-    settings = []
-
-    # Try different API keys to for different providers
-    values.setdefault("openai_api_key", os.getenv("OPENAI_API_KEY"))
-    values.setdefault("ollama_base_url", os.getenv("OLLAMA_BASE_URL"))
-    
-    if api_key := values.get("openai_api_key"):
-        logger.debug("Using OpenAI API key from environment variable")
-        settings.append(OpenAISettings(
-            name="Auto detected OpenAI",
-            api_key=api_key,
-        ))
-
-    if api_base_url := values.get("ollama_base_url"):
-        # TODO: Implement fully, so model and possible key is also detected
-        settings.append(OllamaSettings(
-            api_base_url=api_base_url,
-        ))
-
-
-    return settings
-
-
 class GeneratorSettings(BaseSettings, ABC):
     name: str = Field(..., description="Name of the generator.")
     provider: str = Field(..., description="Provider of the generator.")
@@ -96,7 +65,8 @@ class OpenAISettings(GeneratorSettings):
 class OllamaSettings(GeneratorSettings):
     provider: str = "ollama"
     model: str = Field(..., description="Ollama model.")
-    api_base_url: AnyHttpUrl = Field('http://localhost:11434/api', description="Ollama API base URL.", alias="ollama_base_url")
+
+    url: AnyHttpUrl = Field('http://ollama:11434/api', description="Ollama API base URL.")
     generation_kwargs: Optional[dict] = Field({
         "temperature": 0.0,
     }, description="Ollama generation kwargs.")
@@ -114,3 +84,82 @@ class OllamaSettings(GeneratorSettings):
 #     provider: str = Field("together")
 #     api_key: str = Field(..., description="Together API key.")
 #     model: str = Field(..., description="Together model.")
+
+
+
+def detect_generators(values: dict):
+    """
+    Detect the generator settings based on the environment variables or provided values.
+
+    This function checks for the presence of environment variables or values in the
+    provided dictionary to determine the appropriate generator settings.
+    It sets the default provider based on the detected settings.
+    """
+    settings = []
+
+    # Try different API keys to for different providers
+    values.setdefault("openai_api_key", os.getenv("OPENAI_API_KEY"))
+    values.setdefault("ollama_host", os.getenv("OLLAMA_HOST"))
+    values.setdefault("ollama_host", os.getenv("OLLAMA_BASE_URL"))  # open-webui compatible
+    values.setdefault("ollama_model", os.getenv("OLLAMA_MODEL"))
+    
+    if api_key := values.get("openai_api_key"):
+        logger.debug("Using OpenAI API key from environment variable")
+        settings.append(OpenAISettings(
+            name="Auto detected OpenAI",
+            api_key=api_key,
+        ))
+
+    if api_base_url := values.get("ollama_host"):
+        print(f"Using Ollama API base URL from environment variable: {api_base_url}")
+
+        # Try to detect the model from the environment variable
+        model = values.get("ollama_model") or _pull_default_ollama_model(api_base_url)
+        if model:
+            name = f"Auto detected Ollama {model}"
+            settings.append(OllamaSettings(
+                name=name,
+                url=api_base_url,
+                model=model,
+            ))
+
+    return settings
+
+
+def _pull_default_ollama_model(api_base_url: str) -> Optional[str]:
+    """
+    Pull the default model from the Ollama API.
+
+    This function makes a request to the Ollama API to get the list of models
+    and returns the first model found. If the API response is invalid or
+    empty, it returns None.
+
+    There is no "default" model in the API, so we just return the first one
+    that is running or loaded. If there are no models running, we return the first
+    model that is available.
+    """
+    import requests
+
+    def get_first_model(url):
+        """
+        Get the first model from the API response.
+        """
+        try:
+            response = requests.get(url, timeout=5)
+            if response.ok:
+                data = response.json().get("models", [])
+                print(data)
+                if data:
+                    return data[0].get("model")
+        except requests.RequestException as e:
+            logger.error("Error fetching models from %s: %s", url, e)
+            return None
+        except ValueError:
+            logger.error("Invalid response from %s", url)
+            return None
+
+    # Return first running model, or just the first available model
+    return get_first_model(f"{api_base_url}/api/ps") \
+        or get_first_model(f"{api_base_url}/api/tags") \
+        or None
+
