b3b8a42cf9463481ce44737b4da3cfde656797ae
Juho Tarkkanen <50268629+trkks@users.noreply.github.com>
2025-09-16 22:39:20 +0300

    feat+refactor: Run update filtering before title processing but it does not seem to work; Reorganize steps into subroutines

--- src/meri/__main__.py
+++ src/meri/__main__.py
@@ -13,10 +13,14 @@ import requests
 from .extractor._processors import MarkdownStr
 from meri.settings import settings
 from meri.extractor._processors import process
-from meri.abc import ArticleTitleResponse
+from meri.abc import ArticleTitleResponse, Article
 from .utils import setup_logging, setup_tracing
 from structlog import get_logger
 from .scraper import extractor
+from .extractor.iltalehti import Iltalehti
+from .pipelines.title import TitlePredictor
+from meri.extractor._extractors import trafilatura_extractor
+
 
 try:
     import rich_click as click
@@ -42,39 +46,64 @@ def cli(cache: bool):
         tmp_dir = tempfile.gettempdir()
         requests_cache.install_cache(f'{tmp_dir}/klikkikuri_requests_cache', expire_after=3600)
 
-ArticleTitleData = tuple[str, ArticleTitleResponse]
-"""Means (article_url, obj)"""
+ArticleTitleData = tuple[Article, ArticleTitleResponse]
+"""Needed for passing article URLs along with title processing results"""
 
 RahtiEntry = ...
 """Placeholder for future"""
 
 
+def hash_url(url: str) -> str:
+    """Placeholder for the real Suola implementation""" 
+    # TODO: Figure out this Wasm mess.
+    # TODO: Wasm thingy.
+    #sign = suola.exports.GetSignature(link)
+    #suola = Instantiate("./suola/build/wasi.wasm")
+    return hashlib.sha256(bytes(url, encoding="utf-8")).hexdigest()
+
+
+def fetch_articles() -> list[Article]:
+    processor = Iltalehti()
+    links = processor.latest()
+    pprint(links)
+
+    articles = []
+    for url in links:
+        article_object = trafilatura_extractor(url)
+        articles.append(article_object)
+    return articles
+
+
+def process_titles(articles: list[Article]) -> list[ArticleTitleData]:
+    predictor = TitlePredictor()
+    results = []
+    for article in articles:
+        result = predictor.run(article)
+        results.append((article, result))
+    return results
+ 
+
 def convert_for_publish(results: list[ArticleTitleData]) -> list[RahtiEntry]:
     """Convert results into the public Klikkikuri data format"""
-    # TODO: Figure out this Wasm mess.
-    #suola = Instantiate("./suola/build/wasi.wasm")
     entries = []
-    for url, title_obj in results:
-        # TODO: Wasm thingy.
-        #sign = suola.exports.GetSignature(link)
-        sign = hashlib.sha256(bytes(url, encoding="utf-8")).hexdigest()
-        # FIXME: This should be the article's publish/edit time, now Meri processing time.
-        updated = str(datetime.now(timezone.utc))
+    for article, title_obj in results:
+        urls = []
+        for url in article.urls:
+            sign = hash_url(str(url.href))
+            urls.append({
+                "labels": [
+                    # TODO: Replace this default.
+                    "com.github.klikkikuri/link-rel=canonical"
+                ],
+                "sign": sign,
+            })
+        updated = str(article.updated_at.replace(tzinfo=timezone.utc))
         title = title_obj.title
         clickbaitiness = title_obj.original_title_clickbaitiness
 
         entries.append({
             "updated": updated,
-            "urls": [
-                # TODO: Gather all article's urls here.
-                {
-                    "labels": [
-                        # TODO: Replace this default.
-                        "com.github.klikkikuri/link-rel=canonical"
-                    ],
-                    "sign": sign,
-                }
-            ],
+            "urls": urls,
             "title": title,
             "clickbaitiness": clickbaitiness,
             "labels": [
@@ -86,13 +115,9 @@ def convert_for_publish(results: list[ArticleTitleData]) -> list[RahtiEntry]:
     return entries
 
 
-def store_results(new_entries: list[RahtiEntry]):
-    """
-    Add the result data of a processing run into the existing Rahti storage.
+RahtiData = dict
 
-    NOTE that any matching signatures of entries in existing and new list will
-    be replaced by the ones with the latest time stamp.
-    """
+def fetch_old_data() -> tuple[str, RahtiData]:
     old_data_file_obj = requests.get(
         "https://api.github.com/repos/Klikkikuri/rahti/contents/data.json",
         headers={
@@ -103,13 +128,40 @@ def store_results(new_entries: list[RahtiEntry]):
     ).json()
 
     old_data = json.loads(base64.b64decode(old_data_file_obj["content"]))
-    pprint(old_data)
 
-    old_entries = old_data["entries"]
+    return old_data_file_obj["sha"], old_data
+ 
+
+def filter_outdated(articles: list[Article], old_entries: list[RahtiEntry]) -> list[Article]:
     # When signatures match, use the newer-released article's entry.
     # TODO: Do this filtering __BEFORE__ passing to title prediction.
     # Initially assume that we are just saving the old article already stored
     # and now pulled from storage for possible update.
+    new_articles = []
+    old_signatures = [set(map(lambda url: url["sign"], x["urls"])) for x in old_entries]
+    new_signatures = [set(map(lambda url: hash_url(str(url.href)), x.urls)) for x in articles]
+    pprint(old_signatures)
+    pprint(new_signatures)
+    for i, news in enumerate(new_signatures):
+        for j, olds in enumerate(old_signatures):
+            if olds & news:
+                # Signature match! Dealing with an article already once processed.
+                # NOTE: Assuming the article.updated_at is a naive utc time.
+                new_datetime = articles[i].updated_at.replace(tzinfo=timezone.utc)
+                old_datetime = datetime.fromisoformat(old_entries[j]["updated"])
+                if new_datetime > old_datetime:
+                    # The new object has an updated version of the article, so
+                    # select that instead.
+                    new_articles.append(articles[i])
+                break
+        else:
+            # If this new object has no signatures matching any old one, the
+            # entry is totally new.
+            new_articles.append(articles[i])
+    return new_articles
+
+
+def merge_updates(old_entries: list[RahtiEntry], new_entries: list[RahtiEntry]) -> list[RahtiEntry]:
     entries = old_entries
     old_signatures = [set(map(lambda url: url["sign"], x["urls"])) for x in old_entries]
     new_signatures = [set(map(lambda url: url["sign"], x["urls"])) for x in new_entries]
@@ -118,30 +170,37 @@ def store_results(new_entries: list[RahtiEntry]):
     for i, news in enumerate(new_signatures):
         for j, olds in enumerate(old_signatures):
             if olds & news:
-                # Signature match! Dealing with an article already once processed.
-                if datetime.fromisoformat(new_entries[i]["updated"]) > datetime.fromisoformat(old_entries[j]["updated"]):
-                    # The new object has an updated version of the article, so
-                    # select that instead.
-                    entries[j] = new_entries[i]
+                # At this point, the "new" one should be known to be an updated
+                # version of a previously processed article, and should thus
+                # replace the old version.
+                entries[j] = new_entries[i]
                 break
         else:
             # If this new object has no signatures matching any old one, the
             # entry is totally new.
             entries.append(new_entries[i])
-    pprint(entries)
+    return entries
 
+
+def store_results(hash_of_stored_file: str, entries: list[RahtiEntry]):
+    """
+    Add the result data of a processing run into the existing Rahti storage.
+
+    NOTE that any matching signatures of entries in existing and new list will
+    be replaced by the ones with the latest time stamp.
+    """
     updated = str(datetime.now(timezone.utc))
+
     data = {
         "status": "ok",
         "updated": updated,
         "schema_version": "0.1.0",
-        "entries": entries
+        "entries": entries,
     }
     pprint(data)
 
     # Push the data to Rahti, finishing the processing run.
     encoded_file_content = base64.b64encode(bytes(json.dumps(data, indent=2), encoding="utf-8")).decode("utf-8")
-    file_hash = old_data_file_obj["sha"]
     res = requests.put(
         "https://api.github.com/repos/Klikkikuri/rahti/contents/data.json",
         headers={
@@ -150,14 +209,16 @@ def store_results(new_entries: list[RahtiEntry]):
             "X-GitHub-Api-Version": "2022-11-28",
         },
         json={
-            "message": "feat: Add results of newest processing run",
+            "message": "feat: Add results of newest processing run" \
+                    if entries \
+                    else "feat: Empty the list of entries",
             "committer":
                 {
                     "name": "Tessa Testaaja",
                     "email": "klikkikuri@protonmail.com",
                 },
             "content": encoded_file_content,
-            "sha": file_hash,
+            "sha": hash_of_stored_file,
         }
     )
 
@@ -174,29 +235,31 @@ def fetch(url=None):
     """
     Fetch article from URL.
     """
-    from .extractor.iltalehti import Iltalehti
-    from .pipelines.title import TitlePredictor
-    from meri.extractor._extractors import trafilatura_extractor
-
     # NOTE: Needed env variables:
-    # OPENAI_API_KEY, GITHUB_USER, GITHUB_PASSWORD
-    processor = Iltalehti()
-    links = processor.latest()
-    pprint(links)
+    # OPENAI_API_KEY, GITHUB_TOKEN
+    articles = fetch_articles()
+    print("ARTICLES:"); pprint(articles); print("\n")
+
+    hash_of_stored_file, old_data = fetch_old_data()
+    old_entries = old_data["entries"]
+    pprint(old_entries)
+    # Use this for emptying Rahti while developing.
+    #store_results(hash_of_stored_file, []); return
+
+    new_articles = filter_outdated(articles, old_entries)
+    pprint(new_articles)
 
     # TODO: Run for all links.
-    results = []
-    for url in links[:5]:
-        article_object = trafilatura_extractor(url)
-        pprint(article_object)
-        predictor = TitlePredictor()
-        result = predictor.run(article_object)
-        results.append((url, result))
-    pprint(results)
+    title_data = process_titles(new_articles[:2])
+    pprint(title_data)
 
-    new_entries = convert_for_publish(results)
+    new_entries = convert_for_publish(title_data)
     pprint(new_entries)
-    store_results(new_entries)
+
+    entries = merge_updates(old_entries, new_entries)
+    pprint(new_entries)
+
+    store_results(hash_of_stored_file, entries)
 
 
 if __name__ == "__main__":
