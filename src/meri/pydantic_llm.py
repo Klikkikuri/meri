0908ec420d8294440901ad949285b49dad79169f
Teemu Autto <teemu@rautakuu.org>
2025-12-31 05:39:29 +0000

    feat: update PydanticOutputParser to handle ChatMessage type and improve JSON extraction

--- src/meri/pydantic_llm.py
+++ src/meri/pydantic_llm.py
@@ -11,6 +11,10 @@ from pydantic import BaseModel, ValidationError
 from haystack import component
 from pydantic_core import from_json
 
+from haystack.dataclasses import ChatMessage
+
+from meri.settings import settings
+
 
 RE_JSON_BLOCK = re.compile(r"```json\n(.*?)\n```", re.MULTILINE | re.DOTALL)
 """ Regular expression to extract JSON block from the response. """
@@ -45,32 +49,33 @@ class PydanticOutputParser:
         self.iteration_counter = 0
 
     # Define the component output
-    @component.output_types(valid_replies=List[str], invalid_replies=Optional[List[str]], error_message=Optional[str], model_output=Optional[BaseModel], template=Optional[str])
-    def run(self, replies: List[str]):
+    @component.output_types(valid_replies=List[ChatMessage], invalid_replies=Optional[List[ChatMessage]], error_message=Optional[str], model_output=Optional[BaseModel], template=Optional[str])
+    def run(self, replies: List[ChatMessage]):
         self.iteration_counter += 1
+        msg = replies[0].text
 
         ## Try to parse the LLM's reply ##
         # If the LLM's reply is a valid object, return `"valid_replies"`
         try:
-            response = extract_json(replies[0])
+            response = extract_json(msg)
             if response is None:
                 logger.debug("No JSON block found in the response, trying to parse the whole response.")
 
                 # Remove the <think> block from the response, and hope that the response is valid JSON
-                response = re.sub(RE_THINK_BLOCK, "", replies[0])
+                response = re.sub(RE_THINK_BLOCK, "", msg)
 
             json = from_json(response, allow_partial=True)
             model = self.pydantic_model.model_validate(json)
 
             # If model is thinking model, and we're missing contemplator, add it
-            if hasattr(model, "contemplator") and not model.contemplator:
+            if hasattr(model, "contemplator") and not model.contemplator:  # type: ignore
                 # Extract the <thinking> block from the response
-                think_block = re.search(RE_THINK_BLOCK, replies[0])
+                think_block = re.search(RE_THINK_BLOCK, msg)
                 if think_block:
                     logger.debug("Found <think> block in the response, using it as contemplator", self.iteration_counter)
                     think_text = think_block.group(2).strip()
                     # Add it to the model
-                    model.contemplator = think_text
+                    model.contemplator = think_text  # type: ignore
 
             logger.debug("OutputValidator at Iteration %d: Valid JSON from LLM - No need for looping", self.iteration_counter, extra={"replies": replies})
 
@@ -85,6 +90,6 @@ class PydanticOutputParser:
                 "OutputValidator at Iteration %d: Invalid JSON from LLM - Let's try again.\n"
                 "Output from LLM:\n%s\n"
                 "Error from OutputValidator: %s",
-                self.iteration_counter, replies[0], e
+                self.iteration_counter, msg, e
             )
-            return {"invalid_replies": replies[0], "error_message": str(e)}
+            return {"invalid_replies": msg, "error_message": str(e)}
