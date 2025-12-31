3aa51434952783920d3e8639a2549248f957f74f
Teemu Autto <teemu@rautakuu.org>
2025-12-31 05:42:09 +0000

    feat: add filtering rules for articles in NewsSource model

--- src/meri/settings/newssources.py
+++ src/meri/settings/newssources.py
@@ -26,16 +26,30 @@ class NewsSource(BaseModel):
         default="rss",
         description="Discoverer type to use for this news source. Defaults to 'rss'."
     )
-    extractor: Optional[str] = Field(
-        None,
-        description="Article content extractor (auto-matched by URL if not specified)",
-    )
+    # extractor: Optional[str] = Field(
+    #     None,
+    #     description="Article content extractor (auto-matched by URL if not specified)",
+    # )
 
     enabled: bool = Field(True, description="Enable the news source.")
 
     language: Optional[str] = Field(
         None,
         description="Default language of the news source content, e.g., 'fi-FI' for Finnish.",
+    )
+
+    # Rules to filter articles from this source
+    min_content_length: Optional[int] = Field(
+        300,
+        description="Minimum content length (in characters) for fetched articles. Articles with less content will be ignored.",
+    )
+    max_num_articles: Optional[int] = Field(
+        100,
+        description="Maximum number of articles to fetch from this source during each fetch operation.",
+    )
+    max_age_days: Optional[int] = Field(
+        7 * 3,
+        description="Maximum age of articles (in days) to fetch from this source. Articles older than this will be ignored.",
     )
 
     # weight: Optional[int] = Field(50, description="Weight of the news source. Higher weight means higher priority.")
